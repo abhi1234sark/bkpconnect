035436d5b9e2c7cbae93736ddc2093040418ddf5
abhi1234sark <42135105+abhi1234sark@users.noreply.github.com>
2025-07-09 23:17:44 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = ({ onLogin }) => {
     }
 
     try {
-      const res = await axios.post('https://bkpconnect.onrender.com/auth/login/api', { 
+      const res = await axios.post('https://bkpconnect.onrender.com/auth/login/auth', { 
         username, 
         password 
       });
