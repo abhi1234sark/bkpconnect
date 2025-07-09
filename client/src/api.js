d193756787e25c7fc58270f2d7137e0091dd215d
abhi1234sark <42135105+abhi1234sark@users.noreply.github.com>
2025-07-09 23:07:04 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bkpconnect.onrender.com/',
+  baseURL: 'https://bkpconnect.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
